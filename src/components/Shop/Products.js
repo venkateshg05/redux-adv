e05dc0aeaf5b426e8dba760626536809ec0337d5
venkateshg05 <venkatesh_gopal@outlook.com>
2022-08-14 16:53:23 -0500

    (feat) add more products to site

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,24 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY = [
+  { id: "p1", price: 6, title: "MFB", description: "MFB i authored" },
+  { id: "p2", price: 12, title: "MSB", description: "MSB i authored" },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY.map((product) => (
+          <ProductItem
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
